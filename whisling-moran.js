56f3783c0bfac32ec6c6753df59b5cd935392330
ivymmurithi <ivymurithi@gmail.com>
2021-12-01 12:56:26 +0300

    countdown to whisling moran working

--- whisling-moran.js
+++ whisling-moran.js
@@ -5,21 +5,21 @@ let seconds = document.getElementById("seconds");
 
 let currentYear = new Date().getFullYear();
 
-const raceTrialDay = new Date(`5th December ${currentYear + 1} 00:00:00`);
+const raceTrialDay = new Date(`December 05 2021 ${currentYear + 1} 00:00:00`);
 
 function countDown() {
     const currentTime = new Date();
     const timeBetween = raceTrialDay - currentTime;
 
-    const day = Math.floor(timeBetween / 1000 / 60 / 60 / 24);
-    const hour = Math.floor(timeBetween / 1000 / 60 / 60) %24;
-    const minute = Math.floor(timeBetween / 1000 / 60) % 60;
-    const second = Math.floor(timeBetween / 1000) % 60;
+    const day = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+    const hour = Math.floor((timeBetween / (1000 * 60 * 60)) %24);
+    const minute = Math.floor((timeBetween / 1000 / 60) % 60);
+    const second = Math.floor((timeBetween / 1000) % 60);
 
 days.innerHTML = day;
 hours.innerHTML = hour < 10 ? "0" + hour : hour;
 minutes.innerHTML = minute < 10 ? "0" + minute : minute;
-// seconds.innerHTML = second < 10 ? "0" + second : second;
+seconds.innerHTML = second < 10 ? "0" + second : second;
 }
 
 setInterval(countDown, 1000);
